731a4b0560d4f5935561ef8b7c598d879da43611
jsoul92 <jazzysoul3@gmail.com>
2020-02-25 13:26:52 +0200

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,26 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/IMG_0020.JPG') {
+      myImage.setAttribute ('src','images/IMG_0021.jpg');
+    } else {
+      myImage.setAttribute ('src','images/IMG_0020.JPG');
+    }
+}
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Have a good day, ' + myName;
+}
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Have a good day, ' + storedName;
+}
+myButton.onclick = function() {
+  setUserName();
+}
